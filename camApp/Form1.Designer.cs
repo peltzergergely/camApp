00ac4cb08280eea09b3dc05a9c193f760bc68d78
peltzergergely <peltzer.gergely@gmail.com>
2017-11-16 12:46:13 +0100

    0.5 initial commit
    basic functionality camera application

---
+++ camApp/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace camApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.pause = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(23, 23);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 0;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(104, 23);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(75, 23);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // pause
+            // 
+            this.pause.Location = new System.Drawing.Point(185, 23);
+            this.pause.Name = "pause";
+            this.pause.Size = new System.Drawing.Size(75, 23);
+            this.pause.TabIndex = 2;
+            this.pause.Text = "pause";
+            this.pause.UseVisualStyleBackColor = true;
+            this.pause.Click += new System.EventHandler(this.pause_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(266, 23);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 3;
+            this.save.Text = "save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // pictureBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 5);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(23, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1258, 714);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 7;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.start, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.stop, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.save, 4, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pause, 3, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 720F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1304, 786);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1304, 786);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fénykép készítése";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button pause;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ camApp/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Drawing.Imaging;
+
+namespace camApp
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture capture;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            //if (capture == null)
+            //    capture = new Emgu.CV.VideoCapture(0);
+            capture = new VideoCapture(0);
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameWidth, 1280);
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameHeight, 720);
+            capture.ImageGrabbed += Capture_ImageGrabbed;
+            capture.Start();
+        }
+
+        private void Capture_ImageGrabbed(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            try
+            {
+                Mat m = new Mat();
+                capture.Retrieve(m);
+                pictureBox1.Image = m.ToImage<Bgr, byte>().Bitmap;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            if (capture != null)
+            {
+                capture = null;
+            }
+        }
+
+        private void pause_Click(object sender, EventArgs e)
+        {
+            if (capture != null)
+            {
+                capture.Pause();
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save(@"D://test.jpg", ImageFormat.Jpeg);
+        }
+    }
+}
